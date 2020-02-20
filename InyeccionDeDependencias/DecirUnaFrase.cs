bafaa72fe693e76bce8f63538b5e68b2e03724db
Gabriel Canosa <gabriel.canosa.sp@SofttekProjects.com>
2020-02-20 11:11:32 -0300

    Se agrega inyeccion de dependencias

---
+++ InyeccionDeDependencias/DecirUnaFrase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDeDependencias
+{
+    public class DecirUnaFrase : IDecirUnaFrase
+    {
+        string IDecirUnaFrase.DecirUnaFrase()
+        {
+            return "Tengo la intención de vivir para siempre, o morir en el intento";
+        }
+    }
+}
---
+++ InyeccionDeDependencias/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InyeccionDeDependencias
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+    }
+
+    public class Frase
+    {
+        private IDecirUnaFrase decirUnaFrase;
+
+        public Frase(IDecirUnaFrase _decirUnaFrase)
+        {
+            _decirUnaFrase = decirUnaFrase;
+        }
+
+        public void LlamadoAlMetodoDecirUnaFrase()
+        {
+            decirUnaFrase.DecirUnaFrase();
+            // Ya podemos acceder al metodo DecirUnaFrase() gracias a la inyeccion de dependencias
+            // que realizamos en el constructor
+        }
+    }
+}
